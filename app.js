5b9ff7f25d4dca53c891b1af04d6576c88e96b60
Ahmad Hanif <ahmadmhanif@Ahmads-MBP.lan>
2022-08-04 20:37:33 -0500

    Build the webpage

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-const profielDataArgs = process.argv.slice(2 , process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-console.log(profielDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-
-const printProfileData = (profileDataArr) => {
-
-    // This...
-
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const [nameA, github] = profileDataArgs;
 
 
-    console.log("=====================");
+fs.writeFile('./index.html', generatePage(nameA, github), err => {
+    if (err) throw new Error(err);
 
-    //Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profielDataArgs);
+    console.log('Portfolio complete! Check out the index.html to see the output!')
+});
